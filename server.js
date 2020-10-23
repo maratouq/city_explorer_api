1c9c349f5de66406e3c850c957decf7dd9680c66
hanaa maratouk <hanaamaratouk@gmail.com>
2020-10-23 16:35:25 +0300

    add api call

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let cors = require('cors');
+let superagent = require('superagent');
 
 let app = express();
 app.use(cors());
@@ -11,15 +12,27 @@ const PORT = process.env.PORT;
 app.get('/location', handleLocation);
 
 
-function handleLocation(req,res){
+function handleLocation(req, res) {
+
+  // --->
   let city = req.query.city;
-  let jsonData = require('./data/location.json');
-  let jsonObject = jsonData[0];
-  let locationObject = new Location(city, jsonObject.display_name, jsonObject.lat, jsonObject.lon);
-  res.status(200).json(locationObject);
+  //let jsonData = require('./data/location.json');
+  let key = process.env.GEOCODE_API_KEY;
+  superagent.get(`https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`).then((data) => {
+    let jsonObject = data.body[0];
+    console.log(jsonObject);
+    let locationObject = new Location(city, jsonObject.display_name, jsonObject.lat, jsonObject.lon);
+    res.status(200).json(locationObject);
+  }).catch(() => {
+    res.send('error ... ');
+  });
+  // let jsonObject = jsonData[0];
+  // let locationObject = new Location(city, jsonObject.display_name, jsonObject.lat, jsonObject.lon);
+  // res.status(200).json(locationObject);
+
 }
 
-function Location(search_query, formatted_query, latitude, longitude){
+function Location(search_query, formatted_query, latitude, longitude) {
   this.search_query = search_query;
   this.formatted_query = formatted_query;
   this.latitude = latitude;
@@ -33,6 +46,6 @@ function Location(search_query, formatted_query, latitude, longitude){
 //   "longitude": "-122.332071"
 // }
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`app is listening on port ${PORT}`);
 });
